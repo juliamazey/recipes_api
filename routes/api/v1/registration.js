d6261391915bc59f74d5121014179ed2f8434396
Chris Lewis <13chlewis@gmail.com>
2019-05-10 14:12:22 -0600

    Remove commented out function

--- routes/api/v1/registration.js
+++ routes/api/v1/registration.js
@@ -43,44 +43,4 @@ router.post('/', function(req, res) {
   }
 });
 
-
-// User Registration
-/*
-router.post('/', function(req, res, next){
-  User.findOne({
-    where: {
-      email: req.body.email
-    }
-  })
-  .then(user => {
-    if (req.body.password == req.body.password_confirmation) {
-      if (user == null) {
-        let password = bcrypt.hashSync(req.body.password, 10)
-        let key = Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
-        User.create({
-          email: req.body.email,
-          password: password,
-          api_key: key
-        })
-        .then(user => {
-          res.setHeader("Content-Type", "application/json");
-          res.status(201).send(JSON.stringify( {api_key: user.api_key} ));
-        })
-        .catch(error => {
-          res.setHeader("Content-Type", "application/json");
-          res.status(500).send({ error });
-        });
-      }
-      else {
-        res.setHeader("Content-Type", "application/json");
-        res.status(500).send(JSON.stringify("Email has been taken"));
-      }
-    }
-    else {
-      res.setHeader("Content-Type", "application/json");
-      res.status(500).send(JSON.stringify("Passwords do not match"));
-    }
-  });
-});
-*/
 module.exports = router;

e783525e9379bdbfe2b9e909c3a0c2ab9a8a57f6
Chris Lewis <13chlewis@gmail.com>
2019-05-10 14:08:05 -0600

    Add user registration endpoint, still needs testing

--- routes/api/v1/registration.js
+++ routes/api/v1/registration.js
@@ -1,13 +1,14 @@
-var express = require("express");
-var router = express.Router();
-var User = require('../../../models').User;
+const express = require("express");
+const router = express.Router();
+const User = require('../../../models').User;
 const bcrypt = require('bcrypt');
+const uuidv4 = require('uuid/v4')
 pry = require('pryjs')
 
 router.post('/', function(req, res) {
   if (req.body.password != req.body.password_confirmation) {
     res.setHeader("Content-Type", "application/json");
-    res.status(400).send(JSON.stringify("Passwords do not match"));
+    res.status(400).send(JSON.stringify({ message: "Passwords do not match" }));
   }
   else {
     User.findOne({
@@ -16,22 +17,31 @@ router.post('/', function(req, res) {
     .then(user => {
       if (user) {
         res.setHeader("Content-Type", "application/json");
-        res.status(400).send(JSON.stringify("Email has been taken"));
+        res.status(400).send(JSON.stringify({ message: "Email has been taken" }));
       }
       else {
+        let password = bcrypt.hashSync(req.body.password, 10)
         User.create({
           email: req.body.email,
-          password: '',
-          apiKey: ''
+          password: password,
+          apiKey: uuidv4()
         })
+        .then(user => {
+          res.setHeader("Content-Type", "application/json");
+          res.status(201).send(JSON.stringify( {api_key: user.api_key} ));
+        })
+        .catch(error => {
+          res.setHeader("Content-Type", "application/json");
+          res.status(400).send({ error });
+        });
       }
     })
     .catch(error => {
       res.setHeader("Content-Type", "application/json");
       res.status(400).send({ error });
-    })
+    });
   }
-})
+});
 
 
 // User Registration

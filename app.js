750bafcbe3843f6d6dddc2f116cf8523c8c35aca
Chris Lewis <13chlewis@gmail.com>
2019-05-10 07:23:42 -0600

    Create route for user registration

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
+var registrationRouter = require('./routes/api/v1/registration');
 
 var app = express();
 
@@ -14,5 +15,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
+app.use('/api/v1/users', registrationRouter);
 
 module.exports = app;

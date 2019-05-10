157f427cd42111d42173b6fcff6b02bcde179f5a
Chris Lewis <13chlewis@gmail.com>
2019-05-10 07:23:15 -0600

    Change name column on Users table to email column

--- migrations/20190509215431-create-user.js
+++ migrations/20190509215431-create-user.js
@@ -8,7 +8,7 @@ module.exports = {
         primaryKey: true,
         type: Sequelize.INTEGER
       },
-      name: {
+      email: {
         type: Sequelize.STRING
       },
       password: {
--- seeders/20190509220312-user_seed.js
+++ seeders/20190509220312-user_seed.js
@@ -5,7 +5,7 @@ module.exports = {
     return queryInterface.bulkInsert('Users', [
       {
         id: 1,
-        name: 'User 1',
+        email: 'user1@gmail.com',
         password: 'password',
         apiKey: 'key',
         createdAt: new Date(),
@@ -13,7 +13,7 @@ module.exports = {
       },
       {
         id: 2,
-        name: 'User 2',
+        email: 'user2@gmail.com',
         password: 'password',
         apiKey: 'key',
         createdAt: new Date(),
@@ -21,7 +21,7 @@ module.exports = {
       },
       {
         id: 3,
-        name: 'User 3',
+        email: 'user3@gmail.com',
         password: 'password',
         apiKey: 'key',
         createdAt: new Date(),
@@ -29,7 +29,7 @@ module.exports = {
       },
       {
         id: 4,
-        name: 'User 4',
+        email: 'user4@gmail.com',
         password: 'password',
         apiKey: 'key',
         createdAt: new Date(),
@@ -37,7 +37,7 @@ module.exports = {
       },
       {
         id: 5,
-        name: 'User 5',
+        email: 'user5@gmail.com',
         password: 'password',
         apiKey: 'key',
         createdAt: new Date(),

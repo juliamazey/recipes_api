348bc9ae092836c36a9d6e04cbe4ce070991661f
Julia Marco <marcootj@gmail.com>
2019-05-12 11:42:32 -0600

    Start refactoring routes
    Co-authored-by:  Chris Lewis <41928817+csvlewis@users.noreply.github.com>

---
+++ controllers/login_controller.js
@@ -0,0 +1,33 @@
+const User = require('../models').User;
+pry = require('pryjs');
+const bcrypt = require('bcrypt');
+
+
+const create = (req, res) => {
+  if (req.body.email && req.body.password) {
+    User.findOne({
+      where: { email: req.body.email}
+    })
+    .then(user => {
+      if (bcrypt.compareSync(req.body.password, user.password)) {
+        res.setHeader("Content-Type", "application/json");
+        res.status(200).send(JSON.stringify({ apiKey: user.apiKey }))
+      }
+      else {
+        res.setHeader("Content-Type", "application/json");
+        res.status(401).send(JSON.stringify("Invalid username or password"));
+      }
+    })
+    .catch(error => {
+      res.setHeader("Content-Type", "application/json");
+      res.status(401).send(JSON.stringify({error: "Invalid username or password"}));
+    })
+  }
+  else {
+    res.setHeader("Content-Type", "application/json");
+    res.status(401).send(JSON.stringify({error: "You need to send a password and email"}));
+  }
+}
+
+
+module.exports = { create };
--- routes/api/v1/login.js
+++ routes/api/v1/login.js
@@ -1,35 +1,11 @@
-var express = require("express");
-var router = express.Router();
-var User = require('../../../models').User;
+const express = require("express");
+const router = express.Router();
+const User = require('../../../models').User;
+const loginController = require('../../../controllers/login_controller')
 const bcrypt = require('bcrypt');
 pry = require('pryjs')
 
+router.post('/', loginController.create);
 
-/*POST new session*/
-router.post('/', function(req, res) {
-  if (req.body.email && req.body.password) {
-    User.findOne({
-      where: { email: req.body.email}
-    })
-    .then(user => {
-      if (bcrypt.compareSync(req.body.password, user.password)) {
-        res.setHeader("Content-Type", "application/json");
-        res.status(200).send(JSON.stringify({ apiKey: user.apiKey }))
-      }
-      else {
-        res.setHeader("Content-Type", "application/json");
-        res.status(401).send(JSON.stringify("Invalid username or password"));
-      }
-    })
-    .catch(error => {
-      res.setHeader("Content-Type", "application/json");
-      res.status(401).send(JSON.stringify({error: "Invalid username or password"}));
-    })
-  }
-  else {
-    res.setHeader("Content-Type", "application/json");
-    res.status(401).send(JSON.stringify({error: "You need to send a password and email"}));
-  }
-});
 
 module.exports = router;

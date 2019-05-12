82bccec66ef15d50d48f3ed6f27923c6fbbac469
Chris Lewis <13chlewis@gmail.com>
2019-05-12 12:13:43 -0600

    Refactor GET recipe by dish type endpoint into recipes controller

---
+++ controllers/recipes_controller.js
@@ -0,0 +1,54 @@
+const Recipe = require('../models').Recipe;
+const fetch = require('node-fetch');
+pry = require('pryjs');
+
+// GET recipe by dish type
+const show = (req, res) => {
+  if (req.query.dish_type && req.query.search){
+    getRecipe(req.query.dish_type, req.query.search)
+    .then(function(fetched_recipe){
+      createRecipe(fetched_recipe)
+      .then(recipe => {
+        res.status(200).send(recipe);
+      });
+    })
+    .catch(error => {
+      res.setHeader("Content-Type", "application/json");
+      res.status(400).send({ error });
+    })
+  }
+  else {
+    res.setHeader("Content-Type", "application/json");
+    res.status(400).send({ error: "You need to add a dish type and recipe name" });
+  }
+}
+
+// Helper Functions
+function getRecipe(dish_type, search_query) {
+  var url = `https://api.edamam.com/search?q=${search_query}&app_id=${process.env.app_id}&app_key=${process.env.app_key}&dish_type=${dish_type}`
+  var fetched = fetch(url)
+  .then(function(location_response){
+    return location_response.json();
+  })
+  return fetched
+};
+
+function createRecipe(recipe) {
+  return new Promise(function(resolve,reject) {
+    Recipe.create({
+      name: recipe.hits[0].recipe.label,
+      dishType: recipe.params.dish_type[0],
+      image: recipe.hits[0].recipe.image,
+      recipeUrl: recipe.hits[0].recipe.url,
+      dietLabels: recipe.hits[0].recipe.dietLabels,
+      healthLabels: recipe.hits[0].recipe.healthLabels,
+      ingredientList: recipe.hits[0].recipe.ingredientLines,
+      calories: recipe.hits[0].recipe.calories,
+      cookingTime: recipe.hits[0].recipe.totalTime
+    })
+    .then(rec =>{resolve(rec)})
+    .catch(() => {})
+  })
+};
+
+module.exports = { show };

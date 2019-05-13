edd54a6d9a52fac5845012587bb284c5ea57965b
Julia Marco <marcootj@gmail.com>
2019-05-13 13:39:47 -0600

    Added helper functions to userrecipe model

--- models/userrecipe.js
+++ models/userrecipe.js
@@ -12,13 +12,8 @@ module.exports = (sequelize, DataTypes) => {
     UserRecipe.belongsTo(models.Recipe);
   };
 
-  UserRecipe.saveById = function(UserId, RecipeId, res){
-    UserRecipe.findOrCreate({
-      where: {
-        UserId: UserId,
-        RecipeId: RecipeId
-      }
-    })
+  UserRecipe.saveById = function(userId, recipeId, res){
+    addRecipeToUser(userId, recipeId)
     .then(userRecipe => {
       response.statusMessage(res, 201, 'Recipe has been saved!');
     })
@@ -27,24 +22,42 @@ module.exports = (sequelize, DataTypes) => {
     });
   }
 
-  UserRecipe.deleteById = function(UserId, RecipeId, req, res){
-    UserRecipe.destroy({
-      where: {
-        UserId: UserId,
-        RecipeId: RecipeId
-      }
+  function addRecipeToUser(userId, recipeId){
+    return new Promise(function(resolve, reject) {
+      UserRecipe.findOrCreate({
+        where: {
+          UserId: userId,
+          RecipeId: recipeId
+        }
+      })
+      .then(userRec =>{resolve(userRec)})
+      .catch(error => {reject(error)})
     })
+  }
+
+  UserRecipe.deleteById = function(userId, recipeId, res){
+    var err = `No recipe found with id ${recipeId}`
+    deleteUserRecipe(userId, recipeId)
     .then(recipe => {
-      if (recipe === 0) {
-        response.statusMessage(res, 404, `No recipe found with id ${req.params.id}`);
-      }
-      else {
-        response.status204(res);
-      }
+      recipe === 0 ? response.statusMessage(res, 404, err) : response.status204(res)
     })
     .catch(error => {
-      response.statusMessage(res, 404, `No recipe found with id ${req.params.id}`);
+      response.statusMessage(res, 404, err);
     });
   };
+
+  function deleteUserRecipe(userId, recipeId) {
+    return new Promise(function(resolve, reject) {
+      UserRecipe.destroy({
+        where: {
+          UserId: userId,
+          RecipeId: recipeId
+        }
+      })
+      .then(userRec =>{resolve(userRec)})
+      .catch(error => {reject(error)})
+    })
+  }
+
   return UserRecipe;
 };
